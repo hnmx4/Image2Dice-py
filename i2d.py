0cd56b3fc17f40fe2694ab5b1834e6d266419f20
hnmx4 <hnmnty@gmail.com>
2017-06-15 23:55:25 +0900

    画素値の表示を追加

--- i2d.py
+++ i2d.py
@@ -2,9 +2,23 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('lenna.jpg', 0)
-plt.imshow(img, cmap='gray', interpolation='bicubic')
-plt.show()
 
-# trimming
-# be mozaic
+def trimming(img, mag):
+    height, width = img.shape[:2]
+    return cv2.resize(img, (width - width % mag, height - height % mag), interpolation=cv2.INTER_CUBIC)
+
+
+def mosaic(img, mag):
+    h, w = img.shape[:2]
+    for i in range(0, h, mag):
+        for j in range(0, w, mag):
+            print(img[i, j])
+
+
+img = cv2.imread('lenna.jpg', 0)
+mag = 12
+trim_img = trimming(img, mag)
+mosaic(trim_img, mag)
+
+plt.imshow(trim_img, cmap='gray', interpolation='bicubic')
+plt.show()
